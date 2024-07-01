a10be5035ebc0672452bce34d226fb287aa5662b
Котенев Юрий Павлович (4066290) <ukotenev@vtb.ru>
2024-07-01 18:46:38 +0400

    create user and card schemas

---
+++ src/models/card.ts
@@ -0,0 +1,39 @@
+import mongoose, { Schema } from 'mongoose';
+import { IUser } from './user';
+
+interface ICard {
+  name: string;
+  link: string;
+  owner: IUser;
+  likes: Array<IUser>;
+  createdAt: Date
+}
+
+const cardSchema = new Schema<ICard>({
+  name: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 30,
+  },
+  link: {
+    type: String,
+    required: true,
+  },
+  owner: {
+    type: Schema.Types.ObjectId,
+    ref: 'user',
+    required: true,
+  },
+  likes: [{
+    type: Schema.Types.ObjectId,
+    ref: 'user',
+    required: true,
+  }],
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+export default mongoose.model<ICard>('card', cardSchema);
