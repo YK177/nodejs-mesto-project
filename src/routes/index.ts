f2496f6497193553e9f2990b97fc6634ec581f96
Котенев Юрий Павлович (4066290) <ukotenev@vtb.ru>
2024-10-04 20:37:35 +0300

    handle not found routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import { constants } from 'http2';
 import usersRouter from './users';
 import cardsRouter from './cards';
 
@@ -6,5 +7,8 @@ const router = Router();
 
 router.use('/users', usersRouter);
 router.use('/cards', cardsRouter);
+router.use((_req, res, _next) => res
+  .status(constants.HTTP_STATUS_NOT_FOUND)
+  .send({ message: 'Запрашиваемый ресурс не найден' }));
 
 export default router;
